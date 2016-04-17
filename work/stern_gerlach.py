dbea34d6d45f1a15008aec0da598bf221a2ace1e
Andrew <andrewdurkiewicz@gmail.com>
2016-04-16 20:34:13 -0700

    'I fixed everything as far as I can tell. I realized that the spin must be from -1 to 1...will post in wiki on this. I also incorporated in the initial conditions such as small variations in the v's in the x and z and even y direction. This gives us a more realistic approach to this. I moved the 'screen' back and made it so the particles traves some time after leaving the field with no force. Next step is live plotting as well as making sure that we can set the boundaries to -1 and 1.'

--- work/stern_gerlach.py
+++ work/stern_gerlach.py
@@ -4,43 +4,86 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def dynamics(mass=1, N=1000, alpha=1, gamma=1,
-             vy=100, vx=20, y_end=10):
+             vy=100, vx=20, B_end=10, Y_end = 40):
     # We are making a range of values between
     # -1/2 and 1/2 for possible spin values in the z direction
-    L_z = np.random.uniform(-1, 1, N)
-    L_x = np.random.choice([-1, 1], N) * np.sqrt(1-L_z**2)
+    #Classical mechanics says nothing against the idea of spin having to be between
+    #the values of -1/2 and 1/2 , the way we will interpret this from here on is that
+    #the radius of the Spin-space is = 1, not sqrt(3)/2, this makes it much better as
+    #well as giving us the result we wanted .Think about it to, if we sent a particle 
+    #with spin = 0 in the z direction, then the formula we have fails: 3/4 = s_z^2 +S_y^2 + S_x^2 as
+    #unless increase S_x or S_y above 1/2, we cant equate the sides. This meanst that we are wrong
+    #by contradiction. We thus must make the assumption that we can have an atom with 100% of its spin 
+    #in a single axis direction. This would force the bounds to be -1 to 1: From that, we can solve for S_x
+             
+    S_z   = np.random.uniform(-1, 1, N)
+    S_y   = np.random.uniform(-1, 1, N)
 
+    #we effectively do not need to worry about the S_y since it will not affect any of the motion as it is perpendicular to the 
+    #B field at all locations.But it is however needed to calculate the S_x since they are linearly dependent. 
+
+    S_xpos = np.sqrt(1 - S_y**2 - S_z**2)
+    S_xneg = -np.sqrt(1 - S_y**2 - S_z**2)
     #calculate the force in the z direction, see griffiths page 182 for more info
-    f_z = gamma * alpha*L_z
-    f_x = gamma * alpha*L_x
+    f_z = gamma * alpha*S_z
+    f_xpos = gamma * alpha*S_xpos
+    f_xneg = gamma * alpha*S_xneg
 
-    #force is constant in the z direction, thus there is a constant acceleration. Calculate using F=ma
-    a_z = f_z / mass
-    a_x = f_x / mass
+    #force is constant in the z direction while in the field, thus there is a constant acceleration. Calculate using F=ma
+    a_z     = f_z / mass
+    a_xpos = f_xpos / mass
+    a_xneg = f_xneg / mass
 
     # F = 0 in the y direction, thus we can calculate the
     # time it takes for the atom to go from the beginning of the
-    # magnetic field to the end.
-    t = y_end / vy
+    # magnetic field to the end
+    v0x = np.random.uniform(-.02,vx +.02,N)
+    v0y = np.random.uniform(vy,vy+2,N)    
+    v0z = np.random.uniform(-.02,vx+.02,N)
+    
+    #time to leave the field
+    t = B_end / v0y
+    
+    # Calculate the z,x position as the atom leaves the magnetic field
+    #lf = leaving field
+    z_lf =     v0z*t + .5 * a_z * t**2
+    x_lf_pos = v0x*t + .5 * a_xpos * t**2
+    x_lf_neg = v0x*t + .5 * a_xneg * t**2
+    
 
-    # Calculate the final z position
-    z_final = .5 * a_z * t**2
-    x_final = .5 * a_x * t**2
+    # the magnetic field last 20 units, lets make the screen 10 more units away
+    #in these moments, there will be no acceleration at all, we neeed to recalculate
+    #the velocities in each direction and maintain them through the end
+    
+    v_znew     = v0z + a_z*t
+    v_xpos_new = v0x + a_xpos*t
+    v_xneg_new = v0x + a_xneg*t
+    
+    #calculate the time from the leaving the field to the screen
+    t_new = (Y_end - B_end)/v0y
 
-    return z_final, x_final
+    z_f = z_lf + v_znew*t_new
+    x_fpos = x_lf_pos + v_xpos_new*t_new
+    x_fneg = x_lf_neg + v_xneg_new*t_new
+    
+    
+    return z_f, x_fpos, x_fneg
 
 
 if __name__ == '__main__':
     parameters = {
         'mass':  1,    #Mass of atom (kg)
-        'N':     1000, #Number of atom
+        'N':     20000, #Number of atom
         'alpha': 1,    #Small deviation from homogeneity
         'gamma': 1,    #Gyromagnetic constant
-        'vy':    500,  #Velocity in the y-direction (m/s)
-        'vx':    20,   #Velocity in the x-direction in (m/s)
-        'y_end': 20    #y distance particle will travel
+        'vy':    100,  #Velocity in the y-direction (m/s)
+        'vx':    0,   #Velocity in the x-direction in (m/s)
+        'B_end': 20,    #y distance particle will travel
+        'Y_end': 30, #The screen location after the field
     }
-
-    z, x = dynamics(**parameters)
-    plt.scatter(x, z)
+    
+    z, xp,xm = dynamics(**parameters)
+    
+    plt.scatter(xp ,z)
+    plt.scatter(xm, z)
     plt.show()
