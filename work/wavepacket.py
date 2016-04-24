0df1199baff54ce61c15080d40e80dd476c38534
Greg Vetaw <gvetaw@asu.edu>
2016-04-24 05:59:32 -0700

    I defined the spinor function that we will need for for the psi
    In the script I added the spinor function of the electron before
    it goes through the magnetic field. The spinor function in is the
    inverse fourier transform of the gaussian defined in the text file.
    Also I added a pdf of the plot of the spinor function where the function
    is centered around zero.

--- work/wavepacket.py
+++ work/wavepacket.py
@@ -23,15 +23,47 @@ def gaussian(p,k,beta,n):
     
     return g
 
-def plot_gaussian(gaussian,p, figname="plot_of_gaussian.pdf"):
+def spinor_in(gaussian):
+    
+    ## Here we are defining the spinor function of the electron 
+    ## before it enters the space containing the inhomogeneous magnetic field
+    
+    ## inverse fourier transform of the gaussian
+    
+    return np.fft.ifft(gaussian)
+    
+def plot_spinor_in(spinor_in,N, figname="plot_of_spinor_in.pdf" ):
+    
+    """Plot of spinor_in."""
+    x = np.fft.fftfreq(N) # defines the sample positions for the inverse fourier transform
+    xs = np.fft.ifftshift(x) # shifts the zero position to the center
+    
+    y=(1/N)*(np.abs(np.fft.ifftshift(spinor_in)))**2 # the spionor function is complex
+    fig = plt.figure()                               # so we take the absolute value squared and divide by 1/N
+    ax = fig.add_subplot(111)
+    ax.plot(xs,y)
+    
+    ax.set_xlabel('position')
+    ax.set_ylabel(r'$\Chi_in')
+   
+    
+
+    if figname:
+        fig.savefig(figname)
+        print("Wrote figure to {}".format(figname))
+    
+    
+
+def plot_gaussian(gaussian, figname="plot_of_gaussian.pdf"):
     """Plot of the gaussian."""
     
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.plot(p,gaussian)
     
-    ax.set_xlabel('p')
-    ax.set_ylabel('g')
+    ax.set_xlabel('momentum')
+    ax.set_ylabel('gaussian')
+   
     
 
     if figname:
