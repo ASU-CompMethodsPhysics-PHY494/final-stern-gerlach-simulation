6333504c11004f39588a151116ef9a601210a06e
Andrew Durkiewicz <andrewdurkiewicz@gmail.com>
2016-05-03 13:58:01 -0700

    fixed some cosmetic issues. Nothing impacting the outcome

--- Submission/ClassicalSolution.py
+++ Submission/ClassicalSolution.py
@@ -3,8 +3,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+
 def dynamics(mass=1, N=1000, alpha=1, gamma=1,
              vy=100, vx=20, B_end=10, Y_end = 40,vz = 20):
+                 
+                 
     # We are making a range of values between
     # -1/2 and 1/2 for possible spin values in the z direction
     # Classical mechanics says nothing against the idea of spin having to be between
@@ -23,28 +27,34 @@ def dynamics(mass=1, N=1000, alpha=1, gamma=1,
     # B field at all locations.But it is however needed to calculate the S_x since they are linearly dependent.
 
     S_x = np.random.choice([-1, 1], N) * np.sqrt(1 - S_y**2 - S_z**2)
+    
     # calculate the force in the z direction, see griffiths page 182 for more info
     f_z = gamma * alpha * S_z
     f_x = gamma * alpha * S_x
 
     # force is constant in the z direction while in the field, thus there is a constant acceleration. Calculate using F=ma
+
     a_z    = f_z    / mass
     a_x = f_x / mass
 
     # F = 0 in the y direction, thus we can calculate the
     # time it takes for the atom to go from the beginning of the
     # magnetic field to the end
+
     v0x = np.random.uniform(vx-.02, vx+.02, N)
     v0y = np.random.uniform(vy,   vy+2,   N)
     v0z = np.random.uniform(vz-.02, vz+.02, N)
     
     #time to leave the field
+    
     tbf = .19
     t = B_end / v0y
     x_xi = v0x*tbf
     x_zi = v0z * tbf
+    
     # Calculate the z,x position as the atom leaves the magnetic field
     # lf = leaving field
+    
     z_lf = x_zi + v0z*t + .5 * a_z * t**2
     x_lf = x_xi + v0x*t + .5 * a_x * t**2
 
